9491f53abfbdaad7ffabdb4f8f4869e42badd5bf
Neal Magee <nmagee@gmail.com>
2016-05-13 16:27:22 -0400

    Adding comments to the jobs.py

--- api/python/jobs.py
+++ api/python/jobs.py
@@ -1,12 +1,15 @@
 #!/bin/python
 import requests
 
+# Add your full token here
 token = '12345'
 
-# this should stay the same
+# Take the base URL and then add the token to it
 url = 'https://locals.seiu.org/api/v1/jobs/token/'
 fullurl = url + token
 
+# Make the actual GET request
 r = requests.get(fullurl)
 
+# Echo back the JSON response
 print r.text
