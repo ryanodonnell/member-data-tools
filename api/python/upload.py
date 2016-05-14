ac7964f137982f2fc8badd0d3c267be1775d4199
Neal Magee <neal.magee@seiu.org>
2016-05-14 09:39:20 -0400

    Basic working upload.py example

---
+++ api/python/upload.py
@@ -0,0 +1,58 @@
+#!/bin/python
+import requests
+import rebus
+import pysftp
+import os
+
+'''
+This script does a few things in order to upload a file via SFTP and then make the /upload/ call to the MAPP API:
+ - base64 encodes your filename (this is required since the filename will be part of your API URL
+ - SFTPs the file to SEIU
+ - Assembles the full API call
+ - Makes the API call and retrieves the returned JSON
+
+The script does not do a few key things you should implement:
+ - Error handling or TRY/EXCEPT statements
+ - Logging
+ - Securely storing/passing SFTP credentials and token
+'''
+
+# Your filename
+filename = 'myfile.txt'
+
+# Create the base64 of the filename
+filenameenc = rebus.urlsafe_b64encode(filename)
+filebase64 = filenameenc.rstrip()
+
+# Print these just for feedback to developer
+print("Your filename is: " + filename)
+print("The base64 encoded filename is: " + filebase64)
+
+# FTP the file to SEIU
+# It is never recommended to store credentials directly in code. You should use envvars or a lookup mechanism for this.
+# pysftp also allows for ssh key authentication
+os.chdir('_/PATH/TO/FILE/_')
+seiuhost = 'localsftp.seiu.org'
+seiuusername = '_MYUSERNAME_'
+seiupassword = '_MYPASSWORD_'
+seiuport = '40711'
+with pysftp.Connection(seiuhost, username=seiuusername, password=seiupassword, port=seiuport) as sftp:
+    sftp.chdir('/incoming')
+    sftp.put(filename)
+    sftp.close()
+
+# Add your full token here - again, do not store this directly in code
+token = '12345'
+
+# Flow
+flow = '1' # Values are 1 = Catalist+VAN / 2 = Catalist only
+
+# Take the base URL and then assemble the rest from there
+url = 'https://locals.seiu.org/api/v1/upload/file/'
+fullurl = url + filebase64 + '/flow/' + flow + '/token/' + token
+
+# Make the actual GET request
+r = requests.get(fullurl)
+
+# Echo back the JSON response
+print r.text
